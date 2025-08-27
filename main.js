70de50acb53a4beed798022a5931af8eba10f68f
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-08-27 09:30:20 +0530

    basic setup and skeletons

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator,num1,num2){
+
+
+}
+
+
+function add(){
+
+}
+
+function subtract(){
+    
+}
+
+function multiply(){
+    
+}
+
+function divide(){
+    
+}
