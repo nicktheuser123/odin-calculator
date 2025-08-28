aa099306389cb5800d6e7d95bf1502197f15b5a5
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-08-28 08:38:47 +0530

    added main operator functions

--- main.js
+++ main.js
@@ -4,24 +4,51 @@ let num1;
 let num2;
 let operator;
 
+
+// let operators = document.querySelectorAll(){
+
+// }
+
 function operate(operator,num1,num2){
+console.log(operator,num1,num2);
 
+    switch (operator) {
+        case "+":
+            console.log("+");
+           return add(num1,num2);
+            
+        
+        case "-":
+            return subtract(num1,num2);
+            
+
+        case "x":
+            return multiply(num1,num2);
+        
+        
+        case "\\":
+            return divide(num1,num2);
+           
+
+        default:
+            break;
+    }
 
 }
 
 
-function add(){
-
+function add(a,b){
+    return a+b;
 }
 
-function subtract(){
-    
+function subtract(a,b){
+    return a-b;
 }
 
-function multiply(){
-    
+function multiply(a,b){
+    return a*b;
 }
 
-function divide(){
-    
+function divide(a,b){
+    return a/b;
 }
