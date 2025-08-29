dd0832935392943cec69eb1dd5bbf77f4f62d9ca
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-08-29 08:52:28 +0530

    Pseudo code solution + initial query selectors

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 
-
+let current;
 let num1;
 let num2;
 let operator;
 
+//store
+/*
+create event handlers for numbers and operators
+when number is clicked we just keep updating current and appending numbers
+when an operator is clicked it confirms and sets the number
+    if num1 and num2 are empty put the number in 1
+    else put the number in 2
+    if num1 and 2 are not empty it runs operate 
+        in operate 
+        we set the result to the first number
+        we update the display with the result
+        we clear num 2 
+    else it waits for next input
 
-// let operators = document.querySelectorAll(){
 
-// }
+*/
+ let operators = document.querySelectorAll(".operators")
+ let buttons = document.querySelectorAll(".operators")
+ let display = document.querySelector("#display")
+
+ operators.forEach((node)=>{
+    console.log(node)
+    node.addEventListener("click",(e) =>{
+        
+    })
+ })
+
+function numberClick(){
+
+}
+
+function numberClick(){
+
+}
 
 function operate(operator,num1,num2){
 console.log(operator,num1,num2);
